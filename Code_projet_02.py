84936b18c5ccbec44fc01c6d601b32da99cb214f
quentin laparra <quentin.laparra@gmail.com>
2020-12-07 18:13:37 +0100

    Fonction title

--- Code_projet_02.py
+++ Code_projet_02.py
@@ -2,3 +2,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = "http://books.toscrape.com/catalogue/eragon-the-inheritance-cycle-1_153/index.html"
+
+page = requests.get(url)
+
+"""
+if page.ok:
+	soupe = BeautifulSoup(page.text, "html.parser")
+	title = soupe.find('title')
+	print(title.text)
+
+"""
+
+
+
+def title_one():
+	"""Give the title of the book"""
+	soupe = BeautifulSoup(page.text, "html.parser")
+	title = soupe.find('title')
+	return title
+
+#
