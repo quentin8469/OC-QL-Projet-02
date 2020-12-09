6d71b13f09240da24d25064137f3e268bc91f477
quentin laparra <quentin.laparra@gmail.com>
2020-12-09 15:45:40 +0100

    fonctions

--- Code_projet_02.py
+++ Code_projet_02.py
@@ -1,25 +1,39 @@
-#code projet 02 web scraping
+import re
+import csv
 import requests
 from bs4 import BeautifulSoup
 
 url = "http://books.toscrape.com/catalogue/eragon-the-inheritance-cycle-1_153/index.html"
 
 page = requests.get(url)
-
-"""
-if page.ok:
-	soupe = BeautifulSoup(page.text, "html.parser")
-	title = soupe.find('title')
-	print(title.text)
-
-"""
-
+soupe = BeautifulSoup(page.text, 'html.parser')
 
 
 def title_one():
 	"""Give the title of the book"""
-	soupe = BeautifulSoup(page.text, "html.parser")
-	title = soupe.find('title')
+	title = soupe.find(class_='active')
 	return title
 
-#
+print(title_one().text)
+
+def describ():
+    """give the resum of the book"""
+    describ_01 = soupe.find(string=re.compile("dragon"))
+    return describ_01
+
+print(describ())
+
+def prod_info():
+	""" give productuc information th"""
+	item = soupe.find('th')
+	return item 
+
+print(prod_info().text)
+
+def prod_info_2():
+	""" give productuc information td"""
+	item_one = soupe.find('td')
+	return  item_one
+
+print(prod_info_2().text)
+
