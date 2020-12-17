af72153a32f063d13115ed9b93c802563f326e5a
quentin laparra <quentin.laparra@gmail.com>
2020-12-17 11:31:53 +0100

    maj fonction

--- Code_projet_02.py
+++ Code_projet_02.py
@@ -8,7 +8,9 @@ url = "http://books.toscrape.com/catalogue/eragon-the-inheritance-cycle-1_153/in
 page = requests.get(url)
 soupe = BeautifulSoup(page.text, 'html.parser')
 
+print('\n')#saut de ligne
 
+# function for the title of the book
 def title_one():
 	"""Give the title of the book"""
 	title = soupe.find(class_='active')
@@ -16,6 +18,9 @@ def title_one():
 
 print(title_one().text)
 
+print('\n')#saut de ligne
+
+# function for the descrption of the book / autre solution??
 def describ():
     """give the resum of the book"""
     describ_01 = soupe.find(string=re.compile("dragon"))
@@ -23,17 +28,28 @@ def describ():
 
 print(describ())
 
-def prod_info():
-	""" give productuc information th"""
-	item = soupe.find('th')
-	return item 
+print('\n')#saut de ligne
 
-print(prod_info().text)
+# function for the product information of the book
 
-def prod_info_2():
-	""" give productuc information td"""
-	item_one = soupe.find('td')
-	return  item_one
+def prod_info_th():
+    """give the th"""
+    ths = soupe.findAll('tr')
+    for tr in ths:
+        a = tr.find('th')
+        print(a.text)
+    
+    
+def prod_info_td():
+    """give the td"""
+    tds = soupe.findAll('tr')
+    for tr in tds:
+        a = tr.find('td')
+        print(a.text)
 
-print(prod_info_2().text)
 
+
+print (prod_info_th())
+print('\n')#saut de ligne
+print (prod_info_td())
+print('\n')#saut de ligne
