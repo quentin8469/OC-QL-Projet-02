f84b0317de9b93006ca4935e59b98542dc84b314
quentin laparra <quentin.laparra@gmail.com>
2020-12-23 22:41:19 +0100

    Nouvelles fonctions pour un livre

--- script_one_book.py
+++ script_one_book.py
@@ -50,12 +50,12 @@ def get_All_Data_Book():
     return Book_Data
 
 def writeDataBook(books):
-	''' write the data of the book in a csv file'''
-	headers = ['titre', 'category', 'resum', 'produc', 'image']
-	with open('bookinfos.csv', 'w', encoding='utf-8', newline='') as csvfile:
-		book = csv.writer(csvfile, delimiter='_')
-		book.writerow(headers)
-		book.writerow(books)
+    ''' write the data of the book in a csv file'''
+    headers = ['titre', 'category', 'resum', 'produc', 'image']
+    with open('bookinfos.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        book = csv.writer(csvfile, delimiter='_')
+        book.writerow(headers)
+        book.writerow(books)
 
 
 def main():
@@ -66,54 +66,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
-
-print(main())
-
-"""
-import sys
-import csv
-import requests
-from bs4 import BeautifulSoup
-
-urlBook = 'https://books.toscrape.com/catalogue/eragon-the-inheritance-cycle-1_153/index.html'
-page_Book = requests.get(urlBook)
-soupe = BeautifulSoup(page_Book.text, 'html.parser')
-
-def get_Book_Title():
-    title = soupe.find('h1')
-    return(title.text)
-
-def description_Of_The_Book():
-    resum = soupe.findAll('p')[3]
-    return(resum.text)
-	
-def infos_Produc():
-    my_Produc_Infos ={
-    
-    }
-    my_Produc_Infos['UPC: '] = soupe.findAll('td')[0].text
-    my_Produc_Infos['Product Type : '] = soupe.findAll('td')[1].text
-    my_Produc_Infos['Price (excl. tax) : '] = soupe.findAll('td')[2].text
-    my_Produc_Infos['Price (incl. tax) : '] = soupe.findAll('td')[3].text
-    my_Produc_Infos['Tax : '] = soupe.findAll('td')[4].text
-    my_Produc_Infos['Availability : '] = soupe.findAll('td')[5].text
-    my_Produc_Infos[ 'Number of reviews : '] = soupe.findAll('td')[6].text
-    return my_Produc_Infos
-
-def category():
-    category = soupe.findAll('li')[2]
-    return(category.text)
-
-def image_Url():
-    image_Book = soupe.find('img')
-    path_image = image_Book.get('src')
-    url_image = path_image.replace("../../","http://books.toscrape.com/")
-    return(url_image)
-	
-print(get_Book_Title())
-print(category())
-print(description_Of_The_Book())
-print(infos_Produc())
-print(image_Url())
-"""
