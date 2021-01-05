9afea61d32fef092fd52bd425fd016fdcaf8995b
quentin <grob@DESKTOP-8P9L1N9.localdomain>
2021-01-05 22:02:21 +0100

    suppression fonction csv

--- script_one_book.py
+++ script_one_book.py
@@ -80,28 +80,11 @@ def get_all_data_book():
     return book_data
 
 
-def write_data_book(books):
-    ''' write the data of the book in a csv file'''
-
-    with open('bookinfos.csv', 'w', encoding='utf-8', newline='') as \
-        csvfile:
-        fieldnames = [
-            'Urls',
-            'Categorys',
-            'Titles',
-            'Resum',
-            'Producs_Infos',
-            'Images',
-            ]
-        book = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                              delimiter=';')
-        book.writeheader()
-        book.writerow(books)
-
-
 def main():
+    """à completer"""
+    
     books = get_all_data_book()
-    write_data_book(books)
+    
     return books
 
 
