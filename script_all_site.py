100e75e83d20cb7ad989897a825bd9ef5f470952
quentin <grob@DESKTOP-8P9L1N9.localdomain>
2021-01-06 16:13:43 +0100

    script run for one book and csv

--- script_all_site.py
+++ script_all_site.py
@@ -1,3 +1,14 @@
 import script_one_book as sob
+import script_writedata_csv as swc
+import os
 
-print(sob)
+
+def main():
+    """ general script for scraping"""
+    
+    scrap_book = sob.main()
+    swc.write_data_book(scrap_book)
+    return scrap_book
+
+if __name__=='__main__':
+    main()
