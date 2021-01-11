00dcfda97ad0a0bd884ca46e5dceec3cb4afec34
quentin <grob@DESKTOP-8P9L1N9.localdomain>
2021-01-11 17:23:12 +0100

    modif code

--- script_all_books_for_one_category.py
+++ script_all_books_for_one_category.py
@@ -4,7 +4,8 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
-
+import script_one_book as sob
+import script_writedata_csv as swc
 
 def url_categorys(soupe):
     """ get the books urls in one category"""
@@ -14,7 +15,8 @@ def url_categorys(soupe):
 
     for book in books:
         titles = book.select('a')[1]['href']
-        title = titles.replace('../../../','http://books.toscrape.com/catalogue/')
+        title = titles.replace('../../../',
+                               'http://books.toscrape.com/catalogue/')
         urls_books_list.append(title)
     return urls_books_list
 
@@ -22,16 +24,23 @@ def url_categorys(soupe):
 def main():
     """general function of the script"""
 
-    url_category = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
+    url_category = \
+        'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
     page_book = requests.get(url_category)
     soupe = BeautifulSoup(page_book.content, 'html.parser')
     books_list_url = url_categorys(soupe)
+    for books in books_list_url:
+        book = sob.main(books)
+    
+    #book = sob.main(books_list_url)
+   
     return books_list_url
 
 
 if __name__ == '__main__':
     main()
 
-print (main())
+print(main())
+
 
             
