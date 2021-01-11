2cc0dc1ff8ae414d1ac372efac492969dbc08dc9
quentin <grob@DESKTOP-8P9L1N9.localdomain>
2021-01-11 23:05:31 +0100

    modification fonction main

--- script_all_site.py
+++ script_all_site.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import script_one_book as sob
-import script_writedata_csv as swc
 import script_all_category as sac
-import os
+import script_all_books_for_one_category as sabfoc
+#import script_writedata_csv as swc
+#import os
 
 
 def main():
     """ general script for scraping"""
 
+    scrap_all_cat = sac.main()
+    scrap_books_cat = sabfoc.main()
     scrap_book = sob.main()
-
-    # scrap_category = sac.main()
     # swc.write_data_book(scrap_book)
-
-    swc.write_books_data(scrap_book)
-
+    #swc.write_books_data(scrap_book)
     # swc.write_category_csv(scrap_category)
 
-    return scrap_book
-
+    return  print(srap_book)
 
 if __name__ == '__main__':
     main()
+
+            
