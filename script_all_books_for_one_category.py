de8a2cc0841bbe1c7480b2339fcbe77c577eb8b5
quentin <grob@DESKTOP-8P9L1N9.localdomain>
2021-01-13 01:58:19 +0100

    maj fonction generale

--- script_all_books_for_one_category.py
+++ script_all_books_for_one_category.py
@@ -21,24 +21,27 @@ def url_categorys(soupe):
     return urls_books_list
 
 
-def get_all_url_in_category():
+def get_all_url_in_category(urls_list):
     """general function of the script"""
 
-    books_list =[]
-    url_category = \
-        'https://books.toscrape.com/catalogue/category/books/mystery_3/page-1.html'
+    
+    #url_category = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
+    url_category = urls_list
     page_book = requests.get(url_category)
     soupe = BeautifulSoup(page_book.content, 'html.parser')
     books_list_url = url_categorys(soupe)
+    
+    
+    books_list =[]
     for book in books_list_url:
         infos = sob.get_all_book_info(book)
         books_list.append(infos)
-    swc.write_books_data(books_list)
-   
+    #swc.write_books_data(books_list)
+    
     return books_list
 
 
 if __name__ == '__main__':
     get_all_url_in_category()
 
-print(get_all_url_in_category())
+#print(get_all_url_in_category())
