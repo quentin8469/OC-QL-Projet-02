f01c6f51bb2a3186c05bb88c618a0f5ceb7c23a7
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-01-28 23:41:11 +0100

    maj code

--- structure_projet.py
+++ structure_projet.py
@@ -1,51 +1,60 @@
 
 
 def recuperer_les_informations_pour_un_livre(url_d_un_livre):
-    """ fonction qui stockera les informations d'un livre dans un dictionnaire """
+    """ fonction qui stockera les informations d'un livre dans un dictionnaire
+    besoin de l'url d'un livre """
     
     dictionnaire_des_information_du_livre = {"titre" : "nom du livre", 
                                               "upc" : "09747478",
                                         "categorie" : "action"}
-    print('extraction des données du livre')
+    print('extraction des données du livre_dans_un_dictionnaire')
     return dictionnaire_des_information_du_livre
 
 
 def recuperer_tout_les_livres_pour_une_categories(liste_des_urls_de_tous_les_livres_de_la_categorie):
-    """ fonction qui recupera une liste de dictionnaires avec toutes les inforamtions de chaques livres """
+    """ fonction qui recupera une liste de dictionnaires avec toutes les inforamtions de chaques livres
+    et fournie l'url du livre à la fonction recuperer_les_informations_pour_un_livre """
     
-    liste_de_dictionnaire_des_inforamtions_de_tout_les_livres_dune_page = [{"titre" : "nom du livre", 
+    liste_de_dictionnaires_des_inforamtions_de_tout_les_livres_dune_page = [{"titre" : "nom du livre", 
                                               "upc" : "09747478",
                                         "categorie" : "action"}, {"titre" : "nom du livre", 
                                               "upc" : "09747478",
                                         "categorie" : "action"}]
+    #envoie les urls des livres d'une categorie les uns apres les autres
     for url_d_un_livre in liste_des_urls_de_tous_les_livres_de_la_categorie:
         toutes_les_infos_des_livres_d_une_page = recuperer_les_informations_pour_un_livre(url_d_un_livre)
-
+        # boucle qui remplie la liste avec les dictionnaire de data de chaque livres de la categorie
         for informations_livre in toutes_les_infos_des_livres_d_une_page:
-            liste_de_dictionnaire_des_inforamtions_de_tout_les_livres_dune_page.append(informations_livre)
-    print('extration des dictionnaires')
-    return liste_de_dictionnaire_des_inforamtions_de_tout_les_livres_dune_page
+            liste_de_dictionnaires_des_inforamtions_de_tout_les_livres_dune_page.append(informations_livre)
+    print('extration des dictionnaires de tout les livres de la categories')
+    return liste_de_dictionnaires_des_inforamtions_de_tout_les_livres_dune_page
 
 
-def recuperer_toutes_les_urls_des_livres_de_la_categories(liste_des_pages_de_chaques_categories):
-    """ fonction qui recupera les urls de chaques livres d'un categorie """
+def recuperer_toutes_les_urls_des_livres_de_la_categories(urls_des_pages):
+    """ fonction qui recupere les urls de chaques livres d'une categorie pour fournir la liste à la
+    fonction recuperer_tout_les_livres_pour_une_categories. Recupere une liste de dictionnaire de tout 
+    les livres de chaques categories """
     
+    # liste de toutes les urls des livres par categories utilisées pour recuperer les urls des livres
     liste_des_urls_de_tous_les_livres_de_la_categorie = ['http//livre01', 'http//livre02', 'http//livre03']
-    liste_de_liste_de_dictionnaire_des_livres_de_chaque_categories =[[{}], [{}]]
+    # liste de liste de dictionnaire 
+    liste_de_liste_de_dictionnaire_des_livres_de_chaques_categories =[[{}], [{}]]
+
     liste_de_dictionnaire_des_livres = recuperer_tout_les_livres_pour_une_categories(liste_des_urls_de_tous_les_livres_de_la_categorie)
+    # boucle pour créer la liste_de_liste_de_dictionnaire_des_livres_de_chaque_categories
     for dictionnaires_de_livres in liste_de_dictionnaire_des_livres:
-        liste_de_liste_de_dictionnaire_des_livres_de_chaque_categories.append(dictionnaires_de_livres)
-    print('liste des urls des livres')
+        liste_de_liste_de_dictionnaire_des_livres_de_chaques_categories.append(dictionnaires_de_livres)
+    
     print('liste avec tout les livres')
-    return liste_des_urls_de_tous_les_livres_de_la_categorie
+    return liste_de_liste_de_dictionnaire_des_livres_de_chaques_categories
 
 
 def recuperer_toutes_les_pages_des_categories(liste_de_toutes_les_urls_des_categories):
-    """ fonction qui verifiera si il a y plusieurs page dans l'url de chaque categorie """
+    """ fonction qui verifie si il a y plusieurs page dans l'url de chaque categorie """
     
     liste_des_pages_de_chaques_categories = ['http//categorie-page1', 'http//categorie-page2']
     variable = recuperer_toutes_les_urls_des_livres_de_la_categories(liste_des_pages_de_chaques_categories)
-    print('urls des pages pour une categories')
+    print('urls des pages pour toutes les categories')
     return liste_des_pages_de_chaques_categories
 
 
@@ -54,11 +63,17 @@ def recuperer_toutes_les_urls_des_categories(url_du_site):
     
 
     liste_de_toutes_les_urls_des_categories = ['http//categorie01', 'http//categorie02', 'http//categorie03']
+    liste_des_pages_de_chaques_categories = ['http//categorie-page1', 'http//categorie-page2']
     # resquests avec url_du_site
     # beautifullsoup pour recuperer et stocker les urls dans la liste
-    variable = recuperer_toutes_les_pages_des_categories(liste_de_toutes_les_urls_des_categories)
-    
-    return liste_de_toutes_les_urls_des_categories
+    toute_les_urls_des_categories = recuperer_toutes_les_pages_des_categories(liste_de_toutes_les_urls_des_categories)
+    for pages_categories in toute_les_urls_des_categories:
+        toutes_les_urls_des_pages_de_chaques_categories = recuperer_toutes_les_pages_des_categories(pages_categories)
+        for listes_des_urls_des_pages in toutes_les_urls_des_pages_de_chaques_categories:
+            liste_des_pages_de_chaques_categories.append(listes_des_urls_des_pages)
+
+    print('urls des categories')
+    return liste_des_pages_de_chaques_categories
 
 def main():
     """ fonction principale pour l'execution du script"""
@@ -66,15 +81,16 @@ def main():
     url_du_site = 'https://books.toscrape.com/'
     
     
-    tous_les_livres_de_chaque_categories = recuperer_toutes_les_urls_des_categories(url_du_site)
+    toutes_les_urls_des_pages_de_chaque_categories = recuperer_toutes_les_urls_des_categories(url_du_site)
     
-    #boucle pour stocker les info dans liste_dictionnaires_de_tout_les_livres_de_chaque_categories
+    
     liste_dictionnaires_de_tout_les_livres_de_chaque_categories = [] # liste de dictionnaire à sauvgerder au format csv
-    
-    for informations_des_livres in tous_les_livres_de_chaque_categories:
-        for informations_du_livre in informations_des_livres:
+    #boucle pour stocker les info dans liste_dictionnaires_de_tout_les_livres_de_chaque_categories
+    for urls_des_pages in toutes_les_urls_des_pages_de_chaque_categories:
+        all_books_data = recuperer_toutes_les_urls_des_livres_de_la_categories(urls_des_pages)
+        for informations_du_livre in all_books_data:
             liste_dictionnaires_de_tout_les_livres_de_chaque_categories.append(informations_du_livre)
+    print('scrap de tout ')
 			
-
 if __name__ == '__main__':
     main()
